bd7ff03bfffeb6c747edfd191c1c6d10b9390ca8
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-08-07 20:48:13 +0600

    Tasting canvas.

---
+++ hello-world/canvas-script.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('hello-world-canvas');
+const context = canvas.getContext('2d');
+
+// Blue rectangele
+context.fillStyle = 'blue';
+context.fillRect(10, 40, 30, 70);
+
+// Yellow rectangele
+context.fillStyle = 'yellow';
+context.fillRect(50, 30, 60, 30);
