895b28c6757f3da110e18998068d66dbb6a05f5d
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-08-09 16:39:25 +0600

    Complex shapes.

---
+++ complex-shapes/canvas-script.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+    const canvas = document.getElementById('complex-shapes');
+    const context = canvas.getContext('2d');
+
+    // First Z Object
+    context.beginPath();
+    context.strokeStyle = 'red';
+    context.lineWidth = 15;
+    context.lineCap = 'butt';
+    context.lineJoin = 'miter';
+    context.shadowColor = 'blue';
+    context.shadowOffsetX = 10;
+    context.shadowOffsetY = 10;
+    context.shadowBlur = 5;
+    context.moveTo(60, 80);
+    context.lineTo(160, 80);
+    context.lineTo(80, 180);
+    context.lineTo(180, 180);
+    context.stroke();
+
+    // Second Z Object
+    context.beginPath();
+    context.strokeStyle = 'blue';
+    context.lineWidth = 15;
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.shadowColor = 'yellow';
+    context.shadowOffsetX = 10;
+    context.shadowOffsetY = 10;
+    context.shadowBlur = 5;
+    context.moveTo(340, 80);
+    context.lineTo(240, 80);
+    context.lineTo(340, 180);
+    context.lineTo(240, 180);
+    context.stroke();
+
+    // Third Z Object
+    context.beginPath();
+    context.strokeStyle = 'green';
+    context.lineWidth = 15;
+    context.lineCap = 'square';
+    context.lineJoin = 'bevel';
+    context.shadowColor = 'red';
+    context.shadowOffsetX = 10;
+    context.shadowOffsetY = 10;
+    context.shadowBlur = 5;
+    context.moveTo(420, 80);
+    context.lineTo(520, 80);
+    context.lineTo(440, 180);
+    context.lineTo(540, 180);
+    context.stroke();
+};
+
