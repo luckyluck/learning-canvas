87a59e0a2363259111e71ed6d9c37db68f58e8fe
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-08-11 09:10:22 +0600

    Bar.js. Creating the Chart configuration.

--- bar-chart-lib/bar.js
+++ bar-chart-lib/bar.js
@@ -18,5 +18,56 @@
 'use strict';
 
 function BarChart(targetId, width, height, data) {
-    console.log(arguments);
+    // Base
+    const chart = this;
+
+    // Specify configurations
+    chart.configureChart(targetId, width, height, data);
 }
+
+BarChart.prototype.configureChart = function (targetId, width, height, data) {
+    // Base
+    const chart = this;
+
+    // Global canvas settings
+    chart.setCanvasParameters(targetId, width, height, data);
+
+    // Global chart settings
+    chart.setChartParameters();
+};
+
+BarChart.prototype.setCanvasParameters = function (targetId, width, height, data) {
+    // Base
+    const chart = this;
+
+    // Canvas settings
+    chart.id = targetId;
+    chart.width = width;
+    chart.height = height;
+    chart.data = data;
+};
+
+BarChart.prototype.setChartParameters = function () {
+    // Base
+    const chart = this;
+
+    // Axe configurations
+    chart.axeRatio = 10; // in terms of percentage
+    chart.verticalMargin = (chart.height * chart.axeRatio) / 100;
+    chart.horizontalMargin = (chart.width * chart.axeRatio) / 100;
+    chart.axeColor = '#b1b1b1';
+    chart.axeWidth = 0.75;
+
+    // Label configurations
+    chart.fontRatio = 3; // in terms of percentage
+    chart.fontFamily = 'times';
+    chart.fontStyle = 'normal';
+    chart.fontWeight = '300';
+    chart.fontColor = '#666';
+    chart.verticalFontSize = (chart.height * chart.fontRatio) / 100;
+    chart.horizontalFontSize = (chart.width * chart.fontRatio) / 100;
+
+    // Guideline configurations
+    chart.guidelineColor = '#e5e5e5';
+    chart.guidelineWidth = 0.5;
+};
