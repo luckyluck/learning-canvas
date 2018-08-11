7ef0a1f87526675857a9d45a2bef6a5981043a3e
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-08-11 09:27:04 +0600

    Bar.js. Creating the Canvas and performing pre-operations.

--- bar-chart-lib/bar.js
+++ bar-chart-lib/bar.js
@@ -23,6 +23,9 @@ function BarChart(targetId, width, height, data) {
 
     // Specify configurations
     chart.configureChart(targetId, width, height, data);
+
+    // Pre-operations
+    chart.performPreOperations();
 }
 
 BarChart.prototype.configureChart = function (targetId, width, height, data) {
@@ -70,4 +73,71 @@ BarChart.prototype.setChartParameters = function () {
     // Guideline configurations
     chart.guidelineColor = '#e5e5e5';
     chart.guidelineWidth = 0.5;
+};
+
+BarChart.prototype.performPreOperations = function () {
+    // Base
+    const chart = this;
+
+    // Create canvas
+    chart.createCanvas();
+
+    // Get data
+    chart.hadleData();
+
+    // Prepare data
+    chart.prepareData();
+};
+
+BarChart.prototype.createCanvas = function () {
+    // Base
+    const chart = this;
+
+    // Create canvas
+    const canvas = document.createElement('canvas');
+    canvas.id = chart.id + '-' + Math.random();
+    canvas.width = chart.width;
+    canvas.height = chart.height;
+
+    // Append canvas to target container
+    document.getElementById(chart.id).innerHTML = ''; // clean container
+    document.getElementById(chart.id).appendChild(canvas); // add canvas as a child into the cleaned container
+
+    // Add canvas to chart object
+    chart.canvas = canvas;
+    chart.context = canvas.getContext('2d');
+};
+
+BarChart.prototype.hadleData = function () {
+    // Base
+    const chart = this;
+
+    // Data sets
+    chart.labels = [];
+    chart.values = [];
+
+    // Handle data
+    chart.data.forEach(function (item) {
+        chart.labels.push(item.label);
+        chart.values.push(item.value);
+    });
+};
+
+BarChart.prototype.prepareData = function () {
+    // Base
+    const chart = this;
+
+    // Global variables
+    chart.itemsNum = chart.data.length;
+    chart.maxValue = Math.max.apply(null, chart.values);
+    chart.minValue = Math.min.apply(null, chart.values);
+
+    // Axe specifications
+    chart.verticalAxeWidth = chart.height - 2 * chart.verticalMargin; // bottom and top margins
+    chart.horizontalAxeWidth = chart.height - 2 * chart.horizontalMargin; // left and right margins
+
+    // Label specifications
+    chart.verticalUpperBound = Math.ceil(chart.maxValue / 10) * 10;
+    chart.verticalLabelFreq = chart.verticalUpperBound / chart.itemsNum;
+    chart.horizontalLabelFreq = chart.horizontalAxeWidth / chart.itemsNum;
 };
